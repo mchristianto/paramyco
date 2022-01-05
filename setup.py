87b18a47088f35fd2506fa427e0a5d09adc92c42
michaelchristianto <47887872+mchristianto@users.noreply.github.com>
2022-01-05 16:44:53 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='paramyco',
+    version='0.0.1',
+    author='Michael Christianto',
+    author_email='michayoh@gmail.com',
+    description='Mici package for development only',
+    url='https://github.com/mchristianto/paramyco',
+    license='MIT',
+    packages=['paramyco'],
+    install_requires=['pycryptdome', 'paramiko'],
+)
